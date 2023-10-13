f1cfb4c95d26ee7b1de289c05a22e5b4378017d0
belhadj haitam <steankir.haitam@gmail.com>
2023-10-13 22:17:24 +0100

    my echo function

--- echo.c
+++ echo.c
@@ -6,7 +6,26 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 21:46:55 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/10/13 21:46:56 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/10/13 22:12:55 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+void echo(int argc,char **argv)
+{
+    int i;
+    if(argc <= 1)
+        printf("Enter Argement !!");
+    else
+    {
+        i = 0;
+        while(++i < argc)
+        {
+            printf("%s", argv[i]);
+            if (i < argc - 1)
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
