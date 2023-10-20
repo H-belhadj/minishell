75c450bf9a0a65a614e8bb6ca229a66643531ccb
Haitam <steankir.haitam@gmail.com>
2023-10-20 07:05:07 +0100

    dexter

--- exit.c
+++ exit.c
@@ -6,16 +6,28 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 00:13:39 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/10/14 00:16:42 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/10/20 07:03:01 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void exit()
+int is_digit(char c)
 {
-    printf("Before the exit function\n");
-    exit(0);
-    printf("After the exit function\n");
+    return (c >= '0' && c <= '9');
+}
+
+int digit(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        if(is_digit(str[i]))
+            return (1);
+        i++;
+    }
+    return (0);
 }
