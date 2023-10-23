94aeb7983657c05b8fc4d5f7972c6b9f63ef86e5
belhadj haitam <steankir.haitam@gmail.com>
2023-10-23 16:20:40 +0100

    0.7

--- path.c
+++ path.c
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:59:56 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/10/23 13:59:59 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/10/23 16:20:36 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ char *get_path(char *cmd, t_data_cmd *vars)
 
     i = -1;
     char *path = ft_getenv("PATH", vars);
+    if(!path)
+        return (NULL);
     arr = ft_split(path, ':');
     i = -1;
     while(arr[++i])
     {
         char *cmd_path = ft_strjoin(arr[i], "/");
         tmp = ft_strjoin(cmd_path, cmd);
-        // printf("cmd ==> %s\n", tmp);
         if(access(tmp, F_OK) == 0)
             return (tmp);
     }
     return (NULL);
-    // execute_cmd(cmd);
 }
