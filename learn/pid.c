1bd265813b263762ce152d0e0183b3ef0fe6271d
belhadj haitam <steankir.haitam@gmail.com>
2023-10-25 10:22:51 +0100

    rev

--- learn/pid.c
+++ learn/pid.c
@@ -2,10 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
     int id = fork();
+    if(id == 0)
+        sleep(1);
     printf("Current id :%d, parent id: %d\n", getpid(), getppid());
+    int res = wait(NULL);
+    if(res == - 1)
+        printf("No Children To Wait For\n");
+    else
+        printf("%d Fished execution\n", res);
     return(0);
 }
