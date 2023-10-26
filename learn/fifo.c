f726208ce202ea724f0c642ea1a5bc57710ae560
belhadj haitam <steankir.haitam@gmail.com>
2023-10-26 10:19:27 +0100

    fifo

--- learn/fifo.c
+++ learn/fifo.c
@@ -4,9 +4,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
-    int fd[2];
+    if(mkfifo("myfifo1", 0777) == -1)
+    {
+        if(errno != EEXIST)
+        {
+            printf("Could Not Create Fifo File\n");
+            return(1);
+        }
+    }
+    // usleep(300);
+    printf("opening....\n");
+    int fd = open("myfifo1", O_RDWR);
+    if(fd == -1)
+        return(3);
+    // usleep(3000);
+    printf("Opened\n");
+    int x = 97;
+    if(write(fd, &x, sizeof(x)) == -1)
+    {
+        return (2);
+    }
+    // usleep(3000);
+    printf("write\n");
+    close(fd);
+    // usleep(3000);
+    printf("closed\n");
+    return (0);
 }
